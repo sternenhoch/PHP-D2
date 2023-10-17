de60035c21b09a4523d37484139022a8c8c2b4ed
Razmo <you@example.com>
2023-10-17 16:13:09 +0200

    exo

---
+++ ex_02/ex_02.php
@@ -0,0 +1,2 @@
+<?php
+function my_create_map (){}
---
+++ ex_04/ex_04.php
@@ -0,0 +1,14 @@
+<?php
+function my_password_hash($password){
+    $toto = [1, 2 , 3];
+    
+    return $toto;
+}
+
+$resultat = my_password_hash("coucou");
+var_dump($resultat);
+
+$password = array(
+    'hashed' => "H ghC7(-5TH):",
+    'uniqid' => "xxx",
+    );
